fc08f49917ee8aeebb57cf5bbd622cb6196fd452
Emma <emma@example.com>
2022-03-23 17:45:44 -0700

    lets try again

--- home.py
+++ home.py
@@ -1,26 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask
 
 myapp_obj = Flask(__name__)
 
 @myapp_obj.route("/")
 def home():
         name = {'name':'Carlos'}
-        city_names = [{'city':'Paris'}, {'city':'New york'}, {'city': 'Tokyo'}, {'city':'Livermore'}]
+        city_names = ['Paris', 'New york', 'Tokyo', 'Livermore']
         return'''
-<html>
-    <head>
-        <h1> Welcome''' + name['name'] +'''!</h1>
-    </head>
-    <body> <a href = "https://www.google.com/">not google</a>
-      <div>
-      <ul>
-        {% for cities in city_names %}
-          <li> '''+city_names['city']+''' </li>
-        {% endfor %}
-      </ul>
-      </div>
-    </body>
-    </html> '''
+	<html>
+		<head>
+		<h1> Welcome '''+name['name']+'''!</h1> </head>
+		<body> <a href = "https://www.google.com/">not google</a>
+		<div>
+			<ul>
+			<li> '''+city_names[0]+''' </li> <li> '''+city_names[1]+''' </li>
+			<li> '''+city_names[2]+''' </li> <li> '''+city_names[3]+''' </li>
+			</ul>
+		</div>
+		</body>
+	</html> '''
 
 if __name__ == '__main__':
         myapp_obj.run()
